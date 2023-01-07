3c5ae497adc561242a6ec7a36ee1bb2456a28226
pankaj5090 <pankaj5090@gmail.com>
2023-01-07 17:36:57 +0530

    Initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
+  // const myapiKey = process.env.REACT_APP_NEWS_API;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route
+            path="/about"
+            element={
+              <div className="container my-3">
+                <About />
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-export default App;
